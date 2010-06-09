26b4dd19323cbf24ebb4989136bea82cee7e2ae9
Carl Douglas <carl.douglas@gmail.com>
2010-06-09 22:17:03 +1000

    Refactored specs.

--- ruby/spec/tidy/tidy_spec.rb
+++ ruby/spec/tidy/tidy_spec.rb
@@ -2,26 +2,27 @@ require File.join(File.dirname(__FILE__), "..", "spec_helper")
 
 describe "tidy class methods" do
 
+  subject { Tidy.new }
+
   it "should have a new method" do
-    tidy = Tidy.new
+    Tidy.should respond_to :new
   end
 
   it "should parse a string" do
-    tidy = Tidy.new
-    errors, html = tidy.parse("<html><body>String</body></html>")
-    puts errors.inspect
-    puts html.inspect
+    errors, html = subject.parse("<html><body>String</body></html>")
+    errors.should_not be_nil
+    html.should_not be_nil
   end
 
   it "should parse a file" do
     file = File.new(File.join(File.dirname(__FILE__),'test1.html'))
-    tidy = Tidy.new
-    errors, html = tidy.parse(file)
+    errors, html = subject.parse(file)
+    errors.should_not be_nil
+    html.should_not be_nil
   end
 
-  it "should have an access variable" do
-    tidy = Tidy.new
-    tidy.should respond_to(:access)
+  it "should respond to access" do
+    subject.should respond_to(:access)
   end
 
 end
