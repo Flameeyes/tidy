72775cdee59faa566f886c97b2ea72ab91dc1339
Carl Douglas <carl.douglas@nzx.com>
2010-06-09 14:45:39 +1000

    Specs pass, have C warnings to fix...

--- ruby/ext/tidy/ruby-tidy.c
+++ ruby/ext/tidy/ruby-tidy.c
@@ -2,13 +2,19 @@
 #include "tidy.h"
 #include "buffio.h"
 
+static uint rb_tidyGetByte( TidyInputSource* source );
+static void rb_tidyUngetByte( TidyInputSource* source, uint byteValue );
+static Bool rb_tidyIsEOF( TidyInputSource* source );
+
 static VALUE cTidy;
 
+/* release tidy doc memory */
 static void rb_tidy_free(void *ptr)
 {
   tidyRelease(ptr);
 }
 
+/* create a new tidy doc */
 static VALUE rb_tidy_new(VALUE class, VALUE hash)
 {
   VALUE argv[1];
@@ -19,6 +25,7 @@ static VALUE rb_tidy_new(VALUE class, VALUE hash)
   return tdata;
 }
 
+/* parse the given input and return the tidy output, and the errors */
 static VALUE rb_tidy_parse(VALUE self, VALUE input)
 {
   VALUE array;
@@ -26,41 +33,36 @@ static VALUE rb_tidy_parse(VALUE self, VALUE input)
   TidyDoc tdoc;
   TidyBuffer output;
   TidyBuffer errbuf;
-
+  TidyInputSource source;
   int status          = 0;
-  int contentErrors   = 0;
-  int contentWarnings = 0;
-  int accessWarnings  = 0;
 
   Data_Get_Struct(self, TidyDoc, tdoc);
 
   tidyBufInit( &output );
   tidyBufInit( &errbuf );
 
-  // array = rb_ary_new();
+  array = rb_ary_new();
 
   status = tidySetErrorBuffer( tdoc, &errbuf );
 
-  if (rb_respond_to(input, rb_intern("read"))) {
-    // TODO
-  } else {
-    status = tidyParseString(tdoc, StringValue(input));
+  if (status >= 0) {
+    if (rb_respond_to(input, rb_intern("getc"))) {
+      tidyInitSource(&source, (void *)&input, rb_tidyGetByte, rb_tidyUngetByte, rb_tidyIsEOF);
+      status = tidyParseSource(tdoc, &source);
+    } else {
+      status = tidyParseString(tdoc, StringValuePtr(input));
+    }
   }
 
-  contentErrors   += tidyErrorCount( tdoc );
-  contentWarnings += tidyWarningCount( tdoc );
-  accessWarnings  += tidyAccessWarningCount( tdoc );
+  if (status >= 0)
+    status = tidyCleanAndRepair( tdoc );
+  if (status >= 0)
+    status = tidyRunDiagnostics( tdoc );
+  if (status >= 0)
+    status = tidySaveBuffer( tdoc, &output );
 
-  // if (contentErrors + contentWarnings > 0)
-  //     tidyErrorSummary(tdoc);
-
-  status = tidyCleanAndRepair( tdoc );
-
-  status = tidyRunDiagnostics( tdoc );
-
-  // rb_ary_push(array, );
-
-  array = rb_str_split(rb_str_new2(errbuf.bp), "\n");
+  rb_ary_store(array, 0, rb_str_split(rb_str_new2(errbuf.bp), "\n"));
+  rb_ary_store(array, 1, rb_str_new2(output.bp));
 
   tidyBufFree( &output );
   tidyBufFree( &errbuf );
@@ -76,3 +78,22 @@ Init_tidy()
   rb_define_method(cTidy, "parse", rb_tidy_parse, 1);
 }
 
+static uint rb_tidyGetByte( TidyInputSource* source )
+{
+  VALUE *data = (VALUE *)source->sourceData;
+  VALUE value = rb_funcall(data, rb_intern("getc"), 0, NULL);
+  return NUM2INT(value);
+}
+
+static void rb_tidyUngetByte( TidyInputSource* source, uint byteValue )
+{
+  VALUE *data = (VALUE *)source->sourceData;
+  VALUE value = rb_funcall(data, rb_intern("ungetc"), 1, INT2NUM(byteValue));
+}
+
+static Bool rb_tidyIsEOF( TidyInputSource* source )
+{
+  VALUE *data = (VALUE *)source->sourceData;
+  VALUE value = rb_funcall(data, rb_intern("eof"), 0, NULL);
+  return value == 0 ? no : yes;
+}
--- ruby/spec/tidy/tidy_spec.rb
+++ ruby/spec/tidy/tidy_spec.rb
@@ -8,7 +8,14 @@ describe "tidy class methods" do
 
   it "should parse a string" do
     tidy = Tidy.new
-    result = tidy.parse("<html><body>Hello World!</body></html>")
+    result = tidy.parse("<html><body>String</body></html>")
+    puts result.inspect
+  end
+
+  it "should parse a file" do
+    file = File.new(File.join(File.dirname(__FILE__),'test1.html'))
+    tidy = Tidy.new
+    result = tidy.parse(file)
     puts result.inspect
   end
 
