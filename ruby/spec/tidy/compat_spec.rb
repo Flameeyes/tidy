08d08620f49774aa00b258286ec6a030302311a6
Carl Douglas <carl.douglas@nzx.com>
2010-06-10 11:46:23 +1000

    Added compatibility spec

---
+++ ruby/spec/tidy/compat_spec.rb
@@ -0,0 +1,35 @@
+require File.join(File.dirname(__FILE__), "..", "spec_helper")
+
+describe "tidy class methods" do
+
+  subject { Tidy.new }
+
+  it "should be defined" do
+    defined?(Tidy).should be_true
+  end
+
+  it "should have an open method" do
+    Tidy.should respond_to :open
+  end
+
+  it "should have a clean method" do
+    subject.should respond_to :clean
+  end
+
+  it "should have an errors attribute" do
+    subject.should respond_to :errors
+  end
+
+  it "should have a path attribute" do
+    Tidy.should respond_to :path
+    Tidy.path = "hello world"
+    Tidy.path.should == "hello world"
+  end
+
+  it "should parse a string" do
+    tidy = Tidy.open({}) do |tidy|
+      xml = tidy.clean("<html><body>String</body></html>")
+    end
+  end
+
+end
