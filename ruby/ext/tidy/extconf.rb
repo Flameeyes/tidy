9e1e3a0a5a4f330d7e7e9a622046ca86001d431e
Carl Douglas <carl.douglas@gmail.com>
2010-06-16 21:43:02 +1000

    set the compiler and linker flags necessary to generate code coverage information for gcov

--- ruby/ext/tidy/extconf.rb
+++ ruby/ext/tidy/extconf.rb
@@ -1,5 +1,11 @@
 require 'mkmf'
 
+$CFLAGS ||= ''
+$LDFLAGS ||= ''
+
+$CFLAGS << ' -fprofile-arcs -ftest-coverage '
+$LDFLAGS << ' -fprofile-arcs -ftest-coverage '
+
 dir_config("tidy")
 create_makefile("tidy")
 
