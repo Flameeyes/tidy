0d0f74238a3e6c710ceedb570d08d5370ec61fc8
Carl Douglas <carl.douglas@gmail.com>
2010-07-23 17:49:26 +1000

    Added tidy_steps.rb file, which can be copied into a Rails projects cucumber features/step_definitions directory.

---
+++ ruby/features/step_definitions/tidy_steps.rb
@@ -0,0 +1,20 @@
+require 'tidy'
+
+Given /^(.+) is tidy$/ do |page_name|
+
+  visit path_to(page_name)
+
+  tidy = Tidy.open({:show_warnings => true}) do |tidy|
+    out = tidy.clean(response.body)
+  end
+
+  tidy.errors.scan(/(\d+) warning, (\d+) errors were found!/) do |w,e|
+    warnings = w.to_i
+    errors   = e.to_i
+    unless warnings == 0 && errors == 0
+      raise tidy.errors
+    end
+  end
+
+end
+
